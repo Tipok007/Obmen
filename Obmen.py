56110f688fea1319b1e83c3c44b6c5c3c8c0b5cb
Tipok007 <lost.tank.as@gmail.com>
2024-11-21 19:56:09 +0500

    Создана фунция exchange для получения из интернета курсов валют.

--- Obmen.py
+++ Obmen.py
@@ -6,12 +6,23 @@ import json
 
 
 
+def exchange():
+    code = entry.get().upper()
 
-
-
-
-
-
+    if code:
+        try:
+            response = requests.get(f'https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+                mb.showinfo("Курс обмена", f"Курс к доллару: {exchange_rate:.1f} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка", f'Валюта {code} не найдена')
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка{e}.")
+    else:
+        mb.showwarning("Внимание!!!", "Введите код валюты")
 
 
 
@@ -29,4 +40,3 @@ Button(text = "Получить курс обмена к доллару", comman
 window.mainloop()
 
 
-'''def exchange(): code = entry.get().upper()'''
